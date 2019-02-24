a5326b044e1f55f157b8766428bf3b7aa056a885
analogboy56 <47951684+analogboy56@users.noreply.github.com>
2019-02-24 16:40:52 -0500

    Update BaseballConsumer.py

--- BaseballConsumer/BaseballConsumer.py
+++ BaseballConsumer/BaseballConsumer.py
@@ -312,7 +312,7 @@ class BaseballUpdaterBot:
 
             # You'll now want to load https://statsapi.mlb.com/api/v1/schedule/?sportId=1&teamId=136&date=02/24/2019
             # Changes made to load new API
-            url = "https://statsapi.mlb.com/api/v1/schedule/?sportId=1&teamId=136&date"
+            url = "https://statsapi.mlb.com/api/v1/schedule/?sportId=1&teamId=136&date="
             url = url + todaysGame.strftime("%m/%d/%Y")
 
             while not response:
